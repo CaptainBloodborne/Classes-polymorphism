2e6bf28d9df67eb18f2e90b2abe972597cec2091
Tyukina Arina <arina.tyukina@yandex.ru>
2021-03-02 19:54:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Counter:
+    def __init__(self, values: List[int]):
+        self.values = values
+    # TODO: add your code here
+
+
+if __name__ == "__main__":
+    # check if this code is working
+    print(Counter([1, 2, 3]) + "mississippi")
